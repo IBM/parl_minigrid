cd0fbbdc8088b06ac106885b3a120710aab53091
Junkyu Lee <junkyu.lee@ibm.com>
2022-09-12 20:42:30 -0400

    removed importing non-existing module

--- parl_minigrid/envs/__init__.py
+++ parl_minigrid/envs/__init__.py
@@ -1,5 +1,4 @@
 from parl_minigrid.envs.maze_examples import *
-from parl_minigrid.envs.maze_curriculum import *
 from parl_minigrid.envs.standard_envs import *
 from parl_minigrid.envs.register import MazeRoom_env_dict, register_examples
 
